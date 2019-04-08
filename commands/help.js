44e508d20165072522dc57069ae96c07ad8a2467
Kloppie5 <jeroen.kloppenburg@xs4all.nl>
2019-04-08 22:20:22 +0200

    WHITESPACE REFACTOR

--- commands/help.js
+++ commands/help.js
@@ -9,47 +9,45 @@ module.exports = {
 	run: function(bot, message, args) {
 		if ( args.length > 1 ) {
 			message.channel.send("Invalid arguments.");
-      return
+      		return;
 		}
-    var fields = [];
-    if ( args.length == 1 ) {
-      if (!commands[args[0]]) {
-        message.channel.send("Unable to help with this unknown command.");
-        return;
-      }
-      var command = args[0];
+    	var fields = [];
+    	if ( args.length == 1 ) {
+	      	if (!commands[args[0]]) {
+	        	message.channel.send("Unable to help with this unknown command.");
+	        	return;
+	      	}
+	      	var command = args[0];
 
-      if ( permissions.validate[cm.commands[command].permission_level]( message ) )
-      fields.push({
-            name: `!${command} ${cm.commands[command].args}`,
-            value: cm.commands[command].help
-      });
-    }
+	      	if ( permissions.validate[cm.commands[command].permission_level]( message ) )
+	      	fields.push({
+	            name: `!${command} ${cm.commands[command].args}`,
+	            value: cm.commands[command].help
+	      	});
+	    }
 
-    for ( var command in cm.commands ) {
-      if ( permissions.validate[cm.commands[command].permission_level]( message ) )
-      fields.push({
-            name: `!${command} ${cm.commands[command].args}`,
-            value: cm.commands[command].help
-      });
-    }
+	    for ( var command in cm.commands ) {
+	      	if ( permissions.validate[cm.commands[command].permission_level]( message ) )
+	      	fields.push({
+	            name: `!${command} ${cm.commands[command].args}`,
+	            value: cm.commands[command].help
+	      	});
+	    }
 
-    message.channel.send({embed: {
-      color: 3447003,
-      author: {
-        name: bot.user.username,
-        icon_url: bot.user.avatarURL
-      },
-      title: "Help",
-      description: "Robit is here to help!",
-      fields: fields,
-      timestamp: new Date(),
-      footer: {
-        icon_url: bot.user.avatarURL,
-        text: "© Robit"
-      }
-    }
-  });
-
-  }
+	    message.channel.send({embed: {
+			color: 3447003,
+			author: {
+				name: bot.user.username,
+				icon_url: bot.user.avatarURL
+			},
+			title: "Help",
+			description: "Robit is here to help!",
+			fields: fields,
+			timestamp: new Date(),
+			footer: {
+				icon_url: bot.user.avatarURL,
+				text: "© Robit"
+			}
+	    }});
+  	}
 };
