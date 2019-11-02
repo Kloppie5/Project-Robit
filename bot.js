19eb5bd81673fa2bb71dc8bdc484a752790b8c09
Jeroen Kloppenburg <jeroen.kloppenburg@xs4all.nl>
2019-11-02 22:03:00 +0100

    Reintroduced blame command

--- bot.js
+++ bot.js
@@ -32,10 +32,6 @@ try { var cm = require('./cm.js');
 
 bot.on("ready", () => {
 	bot.user.setPresence({ status: 'online', game: { name: 'with discord.js'} });
-
-	cm.load('funny', './commands/funny.js');
-	cm.load('dndquote', './commands/dndquote.js');
-
 	logger.log(logger.Severity.Info, "Bot is ready");
 });
 
